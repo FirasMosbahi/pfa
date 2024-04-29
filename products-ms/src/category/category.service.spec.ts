323ae09106a4280bbd35c867c5b469e0bb5daccd
firas <firasmosbahi10@gmail.com>
2024-04-29 23:10:34 +0100

    tests implemented

--- products-ms/src/category/category.service.spec.ts
+++ products-ms/src/category/category.service.spec.ts
@@ -1,11 +1,37 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { CategoryService } from './category.service';
+import { InjectModel, MongooseModule } from '@nestjs/mongoose';
+import { Category, CategorySchema } from '../schema/category.schema';
+import { Model } from 'mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import * as Joi from 'joi';
 
 describe('CategoryService', () => {
   let service: CategoryService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [
+        ConfigModule.forRoot({
+          isGlobal: true,
+          validationSchema: Joi.object({
+            PORT: Joi.number().required(),
+            DB_HOST: Joi.string().required(),
+            JWT_SECRET: Joi.string().required(),
+          }),
+          envFilePath: '.env',
+        }),
+        MongooseModule.forRootAsync({
+          imports: [ConfigModule],
+          inject: [ConfigService],
+          useFactory: async (configService: ConfigService) => ({
+            uri: configService.get('DB_HOST'),
+          }),
+        }),
+        MongooseModule.forFeature([
+          { name: Category.name, schema: CategorySchema },
+        ]),
+      ],
       providers: [CategoryService],
     }).compile();
 

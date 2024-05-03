bb627d664832dcf85c3b28051e137cb1691e5003
firas <firasmosbahi10@gmail.com>
2024-05-03 15:59:18 +0100

    add build and push images stages to product ms workflow

--- products-ms/src/main.ts
+++ products-ms/src/main.ts
@@ -1,18 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
-  const configService = app.get(ConfigService);
   app.enableCors({
     origin: '*',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     preflightContinue: false,
     optionsSuccessStatus: 204,
   });
-  await app.listen(configService.get('PORT') ?? 5000);
+  await app.listen(3000);
 }
 bootstrap();

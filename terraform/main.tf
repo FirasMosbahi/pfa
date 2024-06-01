3c9875d8db319922059f78ebc59da87b7c0bf65e
firas <firas.mosbehi@insat.ucar.tn>
2024-06-01 22:03:53 +0100

    add terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -29,3 +29,15 @@ resource "azurerm_kubernetes_cluster" "aks" {
     network_plugin    = "kubenet"
   }
 }
+
+output "subscription_id" {
+  value = data.azurerm_client_config.current.subscription_id
+}
+
+output "client_id" {
+  value = data.azurerm_client_config.current.client_id
+}
+
+output "tenant_id" {
+  value = data.azurerm_client_config.current.tenant_id
+}

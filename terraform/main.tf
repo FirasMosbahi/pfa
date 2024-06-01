85712ff2c106894e84aa2dd41e444da83ba7b514
firas <firas.mosbehi@insat.ucar.tn>
2024-06-01 22:14:30 +0100

    fix azure login

--- terraform/main.tf
+++ terraform/main.tf
@@ -30,6 +30,8 @@ resource "azurerm_kubernetes_cluster" "aks" {
   }
 }
 
+data "azurerm_client_config" "current" {}
+
 output "subscription_id" {
   value = data.azurerm_client_config.current.subscription_id
 }

297e644b137fe4d1b5d222f71a79188d71bd9df9
amine <mohamedamineguesmi@insat.u-carthage.tn>
2024-06-02 21:44:09 +0100

    fix terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,13 +1,8 @@
-resource "azurerm_resource_group" "pfa_rg" {
-  name     = var.resource_group_name
-  location = var.location
-}
-
 resource "azurerm_kubernetes_cluster" "aks" {
   name                = var.cluster_name
   kubernetes_version  = var.kubernetes_version
   location            = var.location
-  resource_group_name = azurerm_resource_group.pfa_rg.name
+  resource_group_name = var.resource_group_name
   dns_prefix          = var.cluster_name
   node_resource_group = var.node_resource_group
 
--- terraform/terraform.tfvars
+++ terraform/terraform.tfvars
@@ -1,6 +1,6 @@
-resource_group_name = "aks_terraform_rg"
+resource_group_name = "pfa"
 location            = "West Europe"
 cluster_name        = "pfaaks"
-kubernetes_version  = "1.28.3"
+kubernetes_version  = "1.28.9"
 system_node_count   = 1
 node_resource_group = "aks_pfa_resources_rg"

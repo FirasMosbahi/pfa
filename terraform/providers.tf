ef5414f481afa408938033b5c0f19d99e6f827c3
amine <mohamedamineguesmi@insat.u-carthage.tn>
2024-06-03 21:34:15 +0100

    fix terraform state

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -9,10 +9,4 @@ terraform {
       version = "~>2.0"
     }
   }
-  backend "azurerm" {
-        resource_group_name  = "tfstate"
-        storage_account_name = "tfstate-storage-account"
-        container_name       = "tfstate"
-        key                  = "terraform.tfstate"
-    }
 }

c935cf96c84931328a92c2e6a59ceda68f088c92
firas <firas.mosbehi@insat.ucar.tn>
2024-06-05 20:22:45 +0100

    fixs

--- auth-ms/handlers/health.go
+++ auth-ms/handlers/health.go
@@ -1,5 +1,9 @@
 package handlers
 
-func healthCheck(c *gin.Context) {
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func HealthCheck(c *gin.Context) {
 	c.JSON(200, gin.H{"message": "Auth microservice is up and running"})
 }

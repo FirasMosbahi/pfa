01978c71b079787ac1541d6994845c7200852e6a
firas <firas.mosbehi@insat.ucar.tn>
2024-06-06 00:45:38 +0100

    pipeline CI CD auth ms demo

--- auth-ms/handlers/health.go
+++ auth-ms/handlers/health.go
@@ -5,5 +5,5 @@ import (
 )
 
 func HealthCheck(c *gin.Context) {
-	c.JSON(200, gin.H{"message": "Auth microservice is up and running demo version"})
+	c.JSON(200, gin.H{"message": "Auth microservice is up and running for demo"})
 }

4cb6abe95f855387374ece6ce653b7d18bb4d8dd
firas <firas.mosbehi@insat.ucar.tn>
2024-06-06 02:58:02 +0100

    demo

--- auth-ms/handlers/health.go
+++ auth-ms/handlers/health.go
@@ -5,5 +5,5 @@ import (
 )
 
 func HealthCheck(c *gin.Context) {
-	c.JSON(200, gin.H{"message": "Auth microservice is up and running for the demo v3"})
+	c.JSON(200, gin.H{"message": "Auth microservice is up and running for the demo v4"})
 }
